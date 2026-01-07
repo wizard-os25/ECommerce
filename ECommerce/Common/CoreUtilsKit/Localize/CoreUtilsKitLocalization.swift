8d4370e16867137498fab07523c9b5154189a039
the_wizard <wizard.os25@proton.me>
2026-01-07 16:35:55 +0700

    common: merge CoreUIKit by hand

--- ECommerce/Common/CoreUtilsKit/Localize/CoreUtilsKitLocalization.swift
+++ ECommerce/Common/CoreUtilsKit/Localize/CoreUtilsKitLocalization.swift
@@ -3,6 +3,10 @@
 import Foundation
 
 public enum CoreUtilsKitLocalization: String {
+    case currency_unit
+    case currency_thousand
+    case currency_million
+    case currency_billion
     case point_unit
     case required_touch_id
     case required_face_id
--- ECommerce/Common/CoreUtilsKit/Utils/Extensions/Foundation/NSObjectExt.swift
+++ ECommerce/Common/CoreUtilsKit/Utils/Extensions/Foundation/NSObjectExt.swift
@@ -33,17 +33,17 @@ extension Encodable {
 }
 
 public extension Encodable {
-    func toDictionary(_ encoder: JSONEncoder = JSONEncoder()) -> [String: Any] {
-        do {
-            let data = try encoder.encode(self)
-            let object = try JSONSerialization.jsonObject(with: data)
-            guard let json = object as? [String: Any] else {
-                let context = DecodingError.Context(codingPath: [], debugDescription: "Deserialized object is not a dictionary")
-                throw DecodingError.typeMismatch(type(of: object), context)
-            }
-            return json
-        } catch {
-            return [:]
-        }
-    }
+//    func toDictionary(_ encoder: JSONEncoder = JSONEncoder()) -> [String: Any] {
+//        do {
+//            let data = try encoder.encode(self)
+//            let object = try JSONSerialization.jsonObject(with: data)
+//            guard let json = object as? [String: Any] else {
+//                let context = DecodingError.Context(codingPath: [], debugDescription: "Deserialized object is not a dictionary")
+//                throw DecodingError.typeMismatch(type(of: object), context)
+//            }
+//            return json
+//        } catch {
+//            return [:]
+//        }
+//    }
 }
